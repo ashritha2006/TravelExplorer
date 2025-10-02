0cf159728612190615d3ffe04b7f57cc674851a4
Vidiyala Ashritha <ashrithavidiyala@gmail.com>
2025-10-02 22:56:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// Copy this file to config.js and fill in your keys.
-// Do NOT commit config.js if your repo is public.
+
 
 window.CONFIG = {
   UNSPLASH_ACCESS_KEY: "wguYPiyfaqY41qlH7zrdTyqTJHqrwHy4YiIAhLyiQT8",
   OWM_KEY: "1630f95922a6c9726d07f356645ff18b",
   OPENTRIPMAP_KEY: "5ae2e3f221c38a28845f05b63fd493dcbebefe8eecfd5ce3e27a0c76",
 };
+
 
 
